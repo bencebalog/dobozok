4b53baf539eb9f7a5220fd5b6fb66f7fde2aff55
balog bence <balogbence991@gmail.com>
2024-03-12 12:59:08 +0100

    Timer

--- src/main/java/com/example/dobozok/HelloController.java
+++ src/main/java/com/example/dobozok/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.dobozok;
 
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -12,14 +13,21 @@ public class HelloController {
 
 
     private Label[][] lt = new Label[10][15];
-
+    private AnimationTimer timer = null;
+    private long tt=0;
     public void initialize(){
         for(int s=0;s<10;s++) for(int o=0;o<15;o++){
             lt[s][o] = new Label();
             lt[s][o].setLayoutX(10+o*64);
             lt[s][o].setLayoutY(10+s*64);
             lt[s][o].setGraphic(new ImageView(new Image(getClass().getResourceAsStream("null.png"))));
-
+            timer = new AnimationTimer() {
+                @Override
+                public void handle(long now) {
+                    //timer=now+"500_000_000";
+                    //if(now==timer) mozgat();
+                }
+            };
             pnJatek.getChildren().add(lt[s][o]);
         }
 
